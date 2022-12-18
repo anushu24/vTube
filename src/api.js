390cf4196204d31955dae7050fdcbccff097bdff
anushu24 <shuklaanu1970@gmail.com>
2022-12-18 14:10:43 +0530

    deployment ready

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ console.log(process.env.REACT_APP_YT_API_KEY)
 const request = axios.create({
    baseURL: 'https://youtube.googleapis.com/youtube/v3/',
    params: {
-      key: '',
+      key: 'AIzaSyD5Farup5-GS2mabfppJISUPs3c2XP004M',
    },
 })
 
